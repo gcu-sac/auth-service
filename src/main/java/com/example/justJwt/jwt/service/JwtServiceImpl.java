6d2ae3dc9e42128fec32c562a2c51ad59410bd94
MoonJungGon <mtbird77@naver.com>
2023-11-07 15:17:39 +0900

    JWT Change

--- src/main/java/com/example/justJwt/jwt/service/JwtServiceImpl.java
+++ src/main/java/com/example/justJwt/jwt/service/JwtServiceImpl.java
@@ -18,13 +18,21 @@ public class JwtServiceImpl {
     private String secretKey = "asdfaesfaef"; // 서명에 사용할 secretKey
     private long exp = 1000L * 60 * 60; // 토큰 사용가능 시간, 1시간
 
+
+
     // 토큰 생성하는 메서드
     public String createToken(User user) { // 토큰에 담고싶은 값 파라미터로 가져오기
+        User test = User.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .build();
+
         return Jwts.builder()
                 .setHeaderParam("typ", "JWT") // 토큰 타입
                 .setSubject("userToken") // 토큰 제목
                 .setExpiration(new Date(System.currentTimeMillis() + exp)) // 토큰 유효시간
-                .claim("user", user) // 토큰에 담을 데이터
+                .claim("user", test) // 토큰에 담을 데이터
                 .signWith(SignatureAlgorithm.HS256, secretKey.getBytes()) // secretKey를 사용하여 해싱 암호화 알고리즘 처리
                 .compact(); // 직렬화, 문자열로 변경
     }
